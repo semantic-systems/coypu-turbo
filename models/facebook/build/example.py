3197ec272e919a089b68b1b793ff8a09b6d4e6e9
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-04-03 15:30:17 +0200

    :sparkles: update main.py

--- models/facebook/build/example.py
+++ models/facebook/build/example.py
@@ -120,4 +120,53 @@ cheese =>""",
 
 
 if __name__ == "__main__":
-    fire.Fire(main)
+    #fire.Fire(main)
+    ckpt_dir= CKPT_PATH
+    tokenizer_path= TOKENIZER_PATH
+    temperature = 0.8
+    top_p = 0.95
+    max_seq_len = 512
+    max_batch_size = 32
+
+    local_rank, world_size = setup_model_parallel()
+    if local_rank > 0:
+        sys.stdout = open(os.devnull, "w")
+
+    generator = load(
+        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
+    )
+
+    prompts = [
+        # For these prompts, the expected answer is the natural continuation of the prompt
+        "I believe the meaning of life is",
+        "Simply put, the theory of relativity states that ",
+        "Building a website can be done in 10 simple steps:\n",
+        # Few shot prompts: https://huggingface.co/blog/few-shot-learning-gpt-neo-and-inference-api
+        """Tweet: "I hate it when my phone battery dies."
+        Sentiment: Negative
+        ###
+        Tweet: "My day has been 👍"
+        Sentiment: Positive
+        ###
+        Tweet: "This is the link to the article"
+        Sentiment: Neutral
+        ###
+        Tweet: "This new music video was incredibile"
+        Sentiment:""",
+                """Translate English to French:
+        
+        sea otter => loutre de mer
+        
+        peppermint => menthe poivrée
+        
+        plush girafe => girafe peluche
+        
+        cheese =>""",
+    ]
+    results = generator.generate(
+        prompts, max_gen_len=256, temperature=temperature, top_p=top_p
+    )
+
+    for result in results:
+        print(result)
+        print("\n==================================\n")
