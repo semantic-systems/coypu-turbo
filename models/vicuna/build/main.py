c1fdcfac511e86cc07eb7bff4f42680848961425
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-04-18 17:45:13 +0200

    :llama: rename facebook to llama

--- models/vicuna/build/main.py
+++ models/vicuna/build/main.py
@@ -1,7 +1,10 @@
-from InstructorEmbedding import INSTRUCTOR
+import json
+import requests
 from flask import Flask, jsonify, request
 from flask_healthz import healthz
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from fastchat.conversation import get_default_conv_template, SeparatorStyle
+from fastchat.serve.inference import compute_skip_echo_len
+
 
 app = Flask(__name__)
 
@@ -24,24 +27,61 @@ app.config.update(
 )
 
 
-@app.route('/', methods=['POST', 'GET'])
+def main(message: str, temperature: float = 0.1, max_new_tokens: int = 1024):
+    model_name = '7B'
+    worker_addr = 'http://localhost:21002'
+
+    conv = get_default_conv_template(model_name).copy()
+    conv.append_message(conv.roles[0], message)
+    conv.append_message(conv.roles[1], None)
+    prompt = conv.get_prompt()
+
+    headers = {"User-Agent": "fastchat Client"}
+    pload = {
+        "model": model_name,
+        "prompt": prompt,
+        "max_new_tokens": max_new_tokens,
+        "temperature": temperature,
+        "stop": conv.sep if conv.sep_style == SeparatorStyle.SINGLE else conv.sep2,
+    }
+    response = requests.post(worker_addr + "/worker_generate_stream", headers=headers,
+            json=pload, stream=True)
+
+    # print(f"{conv.roles[0]}: {message}")
+    print(f"response {response}")
+    output = "Sorry, something went wrong. Please contact Junbo."
+    for chunk in response.iter_lines(chunk_size=8192, decode_unicode=False, delimiter=b"\0"):
+        if chunk:
+            data = json.loads(chunk.decode("utf-8"))
+            skip_echo_len = compute_skip_echo_len(model_name, conv, prompt)
+            output = data["text"][skip_echo_len:].strip()
+            # print(f"{conv.roles[1]}: {output}", end="\r")
+    # print("")
+    return output
+
+
+@app.route('/', methods=['POST'])
 def flask():
     authenticated = False
 
     if 'key' in request.json:
         key = request.json['key']
-        if key == '344LDJTP9M4PVUC8': authenticated = True
+        if key == 'M7ZQL9ELMSDXXE86': authenticated = True
 
     if authenticated == False:
         response = {'error': 'no valid API key'}
         http_code = 401
 
     elif 'prompt' in request.json:
-        model = INSTRUCTOR('hkunlp/instructor-large')
-        sentence = str(request.json['sentence'])
-        instruction = str(request.json['instruction'])
-        embeddings = model.encode([[instruction, sentence]]).tolist()[0]
-        response = {'embeddings': embeddings}
+        prompt = request.json['prompt']
+        temperature = request.json['temperature']
+        max_new_tokens = request.json['max_new_tokens']
+
+        response = main(prompt, temperature, max_new_tokens)
+        response = {'content': response,
+                    'meta': {"turbo_version": "vicuna-13B-8bit-quantized",
+                             "temperature": temperature,
+                             "max_new_tokens": max_new_tokens}}
         http_code = 200
 
     else:
